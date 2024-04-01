ea5c222cbb675854a2640cb0b5a9cfd47b77d818
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-04-01 11:42:39 +0700

    hhh

--- trainers/plot.py
+++ trainers/plot.py
@@ -392,6 +392,7 @@ class PLOT(TrainerX):
         # image.shape == [32, 3, 224, 224]
 
         output = self.model(image)
+        print(output)
         loss = F.cross_entropy(-output, label)
         self.model_backward_and_update(loss)
 
